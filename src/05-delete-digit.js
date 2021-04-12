f6afe1825c2a975927d5ce0cec208747c55a20ff
Fray69 <yura.rogovsky2001@gmail.com>
2021-04-12 19:40:48 +0300

    feat: add func delete digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,18 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const array = [];
+  let poisk = n.toString().split('');
+  let result = 0;
+  for (let i = 0; i <= poisk.length; i++) {
+    poisk = n.toString().split('');
+    poisk.splice(i, 1); // Удаляет 1 элемент по индексу i
+    array.push(Number(poisk.join('')));
+  }
+  array.sort((a, b) => a - b);
+  result = array[poisk.length];
+  return result;
 }
 
 module.exports = deleteDigit;
